ac2b5a35df6ddd4a579166fc75abed7ae2621d6c
Håkon Lervik <lervik.haakon@gmail.com>
2020-08-21 21:59:13 +0200

    Updated test

--- src/com/haklerz/test/Body.java
+++ src/com/haklerz/test/Body.java
@@ -1,17 +1,41 @@
 package com.haklerz.test;
 
+import com.haklerz.truss.Renderer;
 import com.haklerz.truss.Time;
 
 public class Body {
-    private Vec pos, vel;
-    private static final double R = 5;
+    public Vec pos, vel;
+    private static final double R = 3;
+    private static final double F = 0.001;
 
     public Body(double x, double y) {
         this.pos = new Vec(x, y);
         this.vel = new Vec();
     }
 
-    public void update(Time time) {
-        pos.add(Vec.mul(vel, time.getDelta()));
+    public void acc(Time time, Vec a) {
+        vel.add(Vec.mul(a, time.getDelta()));
     }
+
+    public void update(Time time) {
+        vel.mul(1-F);
+        pos.add(Vec.mul(vel, time.getDelta()));
+        if (pos.x - R < 0) {
+            pos.x = 0 + R;
+        }
+        if (pos.x + R > 640) {
+            pos.x = 640 - R;
+        }
+        if (pos.y - R < 0) {
+            pos.y = 0 + R;
+        }
+        if (pos.y + R > 360) {
+            pos.y = 360 - R;
+        }
+    }
+
+    public void draw(Renderer renderer) {
+        renderer.setColor(1, 0, 1);
+        renderer.circle(pos.x, pos.y, R);
+    } 
 }
--- src/com/haklerz/test/NBody.java
+++ src/com/haklerz/test/NBody.java
@@ -1,8 +1,14 @@
 package com.haklerz.test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import com.haklerz.truss.*;
 
 public class NBody implements Game {
+    private List<Body> bodies;
+    private Random rng;
 
     public static void main(String[] args) {
         new Runner(new NBody()).run();
@@ -11,14 +17,50 @@ public class NBody implements Game {
     @Override
     public void setup(Settings settings) {
         settings.setTitle("NBody Simulation");
-        settings.setResolution(640 * 2, 360 * 2);
+        settings.setResolution(640, 360);
+
+        rng = new Random();
+        bodies = new ArrayList<>();
+
+        for (int i = 0; i < 50; i++) {
+            bodies.add(new Body(rng.nextDouble() * 640, rng.nextDouble() * 360));
+        }
     }
 
     @Override
     public void update(Time time) {
+        Body a, b;
+        for (int i = 0; i < bodies.size(); i++) {
+            a = bodies.get(i);
+            for (int j = i + 1; j < bodies.size(); j++) {
+                b = bodies.get(j);
+
+                Vec ab = Vec.sub(b.pos, a.pos);
+                double d = ab.mag() * 0.05;
+
+                ab.div(d * d * d);
+
+                double rep = Math.sin(0.5 * time.getElapsed()) + 2;
+                // System.out.println(rep);
+                if (d < rep) {
+                    ab.mul(-1);
+                }
+
+                a.acc(time, ab);
+                b.acc(time, ab.mul(-1));
+            }
+        }
+
+        for (Body body : bodies) {
+            body.update(time);
+        }
     }
 
     @Override
     public void draw(Renderer renderer) {
+        renderer.clear();
+        for (Body body : bodies) {
+            body.draw(renderer);
+        }
     }
 }
