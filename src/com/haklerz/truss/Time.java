c27fe6e56e0bd51d40056afb4018694932d1866e
Håkon Lervik <lervik.haakon@gmail.com>
2020-09-27 15:40:16 +0200

    Implemented framerate counter

--- src/com/haklerz/truss/Time.java
+++ src/com/haklerz/truss/Time.java
@@ -1,20 +1,29 @@
 package com.haklerz.truss;
 
 public class Time {
+    private static final double SMOOTHING = 100;
+
     private long previousNano;
     private double delta;
+    private double deltaSmooth;
     private double elapsed;
 
     public Time(long startNano) {
         this.previousNano = startNano;
         this.delta = 0;
+        this.deltaSmooth = 0;
         this.elapsed = 0;
     }
 
     void update(long currentNano) {
         delta = (currentNano - previousNano) * 1e-9;
+        deltaSmooth = (deltaSmooth * SMOOTHING + delta) / (1 + SMOOTHING);
         elapsed += delta;
         previousNano = currentNano;
+    }
+
+    public double getFramerate() {
+        return 1 / deltaSmooth;
     }
 
     public double getDelta() {
