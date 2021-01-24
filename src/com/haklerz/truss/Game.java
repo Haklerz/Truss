92f799f909f050b8eaf30cb98f909857f79e8a8b
Håkon Lervik <lervik.haakon@gmail.com>
2021-01-25 00:05:20 +0100

    Simplified the Game interface

--- src/com/haklerz/truss/Game.java
+++ src/com/haklerz/truss/Game.java
@@ -5,7 +5,5 @@ package com.haklerz.truss;
  */
 public interface Game {
 
-    public abstract void init(Setup setup);
-
-    public abstract void loop(Time time, Draw draw);
+    public abstract void loop(Renderer renderer);
 }
