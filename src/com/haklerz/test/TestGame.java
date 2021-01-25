620e91f18eba48bd72083c6aa5283b74c8bb6b8b
Håkon Lervik <lervik.haakon@gmail.com>
2021-01-25 20:42:38 +0100

    Changed the testGame structure

--- src/com/haklerz/test/TestGame.java
+++ src/com/haklerz/test/TestGame.java
@@ -1,28 +1,34 @@
 package com.haklerz.test;
 
+import java.util.Random;
+
 import com.haklerz.truss.Game;
 import com.haklerz.truss.Renderer;
 import com.haklerz.truss.Window;
 
 public class TestGame implements Game {
-
-    int f = 0;
-
     public static void main(String[] args) {
-        new Window("Test", 128, 72, new TestGame()).run();
+        new TestGame();
+    }
+
+    private Window window;
+    private Random rng;
+
+    public TestGame() {
+        this.window = new Window("Test", 640, 360, this);
+        this.rng = new Random();
+
+        window.run();
     }
 
     @Override
     public void loop(Renderer renderer) {
-        int n = renderer.getWidth() * renderer.getHeight();
+        // renderer.clear();
+        rng.setSeed(321873);
 
-        renderer.clear();
-
-        for (int i = 0; i < n; i++) {
-            renderer.setColor(0.5 + 0.5 * Math.sin(0.0007 * f + 0.001 * i), 0.5 + 0.5 * Math.sin(0.001 * f + 0.0007 * i), 0.5 + 0.5 * Math.sin(0.0005 * f + 0.0003 * i));
-            renderer.setPixel(i % n, i / n);
+        for (int i = 0; i < renderer.getWidth() * renderer.getHeight(); i++) {
+            renderer.setColor(rng.nextDouble());
+            renderer.setPixel(i % renderer.getWidth(), i / renderer.getWidth());
         }
-
-        f++;
     }
 }
