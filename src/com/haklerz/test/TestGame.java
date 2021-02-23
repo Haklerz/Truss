74305669ade84147c177884532fcc06ce3d89891
Håkon Lervik <lervik.haakon@gmail.com>
2021-02-23 23:04:32 +0100

    Simplified TestGame

--- src/com/haklerz/test/TestGame.java
+++ src/com/haklerz/test/TestGame.java
@@ -1,34 +1,19 @@
 package com.haklerz.test;
 
-import java.util.Random;
-
 import com.haklerz.truss.Game;
 import com.haklerz.truss.Renderer;
 import com.haklerz.truss.Window;
 
 public class TestGame implements Game {
     public static void main(String[] args) {
-        new TestGame();
+        new Window("Test", 64, 36, new TestGame()).run();
     }
 
-    private Window window;
-    private Random rng;
-
     public TestGame() {
-        this.window = new Window("Test", 640, 360, this);
-        this.rng = new Random();
-
-        window.run();
     }
 
     @Override
     public void loop(Renderer renderer) {
-        // renderer.clear();
-        rng.setSeed(321873);
 
-        for (int i = 0; i < renderer.getWidth() * renderer.getHeight(); i++) {
-            renderer.setColor(rng.nextDouble());
-            renderer.setPixel(i % renderer.getWidth(), i / renderer.getWidth());
-        }
     }
 }
