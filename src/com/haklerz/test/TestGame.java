2ffe03aca5dae7e7981ec470c41c0759d70cb726
Håkon Lervik <lervik.haakon@gmail.com>
2021-02-27 22:17:32 +0100

    Measuring framerate

--- src/com/haklerz/test/TestGame.java
+++ src/com/haklerz/test/TestGame.java
@@ -1,19 +1,48 @@
 package com.haklerz.test;
 
+import java.awt.event.KeyEvent;
+
 import com.haklerz.truss.Game;
+import com.haklerz.truss.Input;
 import com.haklerz.truss.Renderer;
 import com.haklerz.truss.Window;
 
 public class TestGame implements Game {
+
+    private int x, y;
+    private long frameCounter = 0;
+    private long startTime;
+
     public static void main(String[] args) {
-        new Window("Test", 64, 36, new TestGame()).run();
+        new Window("Test", 640, 360, new TestGame()).run();
     }
 
     public TestGame() {
+        this.x = 20;
+        this.y = 20;
     }
 
     @Override
-    public void loop(Renderer renderer) {
+    public void loop(Renderer renderer, Input input) {
+        if (System.nanoTime() - startTime > 1000000000 / 4) {
+            System.out.println("fps: " + frameCounter * 4);
+            frameCounter = 0;
+            startTime = System.nanoTime();
+        }
 
+        renderer.setColor(0, 255, 0);
+        if (input.isMouseButtonDown(1)) {
+            renderer.setPixel(input.getMouseX(), input.getMouseY());
+            renderer.setPixel(input.getMouseX() + 1, input.getMouseY());
+            renderer.setPixel(input.getMouseX() - 1, input.getMouseY());
+            renderer.setPixel(input.getMouseX(), input.getMouseY() + 1);
+            renderer.setPixel(input.getMouseX(), input.getMouseY() - 1);
+        }
+
+        if (input.isMouseButtonDown(3)) {
+            renderer.clear();
+        }
+        
+        frameCounter++;
     }
 }
