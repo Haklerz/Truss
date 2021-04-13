937c9e1affd83fedea9022d84bd3af0f8267ab16
Håkon Lervik <lervik.haakon@gmail.com>
2021-04-13 23:41:20 +0200

    Created DefaultGraphicsConfiguration singleton

---
+++ src/com/haklerz/truss/DefaultGraphicsConfiguration.java
@@ -0,0 +1,19 @@
+package com.haklerz.truss;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsEnvironment;
+
+public class DefaultGraphicsConfiguration {
+    private static GraphicsConfiguration configuration;
+
+    private DefaultGraphicsConfiguration() {
+    }
+
+    public static GraphicsConfiguration getInstance() {
+        if (configuration == null) {
+            configuration = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice()
+                    .getDefaultConfiguration();
+        }
+        return configuration;
+    }
+}
