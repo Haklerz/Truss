1f10a13ffcc1d7bd053ca9dc10a3290b04573b44
Håkon Lervik <lervik.haakon@gmail.com>
2021-04-23 21:16:53 +0200

    Created draw method in texture

--- src/com/haklerz/truss/Texture.java
+++ src/com/haklerz/truss/Texture.java
@@ -1,16 +1,39 @@
 package com.haklerz.truss;
 
-public class Texture {
-    
-    public Texture(String path) {
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Graphics2D;
 
+import javax.imageio.ImageIO;
+
+public final class Texture {
+
+    private BufferedImage image;
+
+    public Texture(String path) {
+        try {
+            image = ImageIO.read(new File(path));
+
+            // TODO: Create compatible image
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            // TODO: Gracefully handle exception
+        }
     }
 
     public Texture() {
+        // TODO: Assign empty compatible image
+    }
 
+    protected Image getImage() {
+        return image;
     }
 
     public void draw(Texture texture, float x, float y) {
-
+        Graphics2D graphics = image.createGraphics();
+        graphics.drawImage(texture.getImage(), x, y, null);
     }
 }
